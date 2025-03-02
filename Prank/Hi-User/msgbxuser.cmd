ed2b80375ccef982bf6699ccf5b56ed4b19c2ccf
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2025-03-02 18:07:53 -0300

    Ps1 to Batch

---
+++ Prank/Hi-User/msgbxuser.cmd
@@ -0,0 +1,1 @@
+powershell.exe -Command "1..10 | ForEach-Object { Start-ThreadJob -ScriptBlock { Add-Type -AssemblyName PresentationFramework; while ($true) { [System.Windows.MessageBox]::Show('Olááá $env:username', 'Spam Alert', 'OK', 'Exclamation') } } }"
